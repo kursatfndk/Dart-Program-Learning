04414e2af6e5b563af4250bdcc2e3a5ececa45de
Kürşat Emre Fındık <147005854+kursatfndk@users.noreply.github.com>
2025-09-22 21:54:58 +0300

    Create number_veri_tip.dart

---
+++ 1_degiskenler_veri_tipleri/number_veri_tip.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  int yas = 23;
+  print(yas);
+  //Pythonda float tipi burada double
+  double sicaklik = 50.7;
+  print(sicaklik);
+  //num hem tam sayı hemde ondalıklı  sayıyı alır.
+  num kilo = 70;
+  print(kilo);
+
+  num kilo1 = 70.8;
+  print(kilo1);
+ // var ifadesi variable ifadesinin kısaltılmışıdır.
+ // değişken türünü belirtmeden işlem yapma imkanı sağlar.
+ // veriyi tanır ve öyle işleme başlar.
+  var yil = 1995;
+  print(yil);
+  // .toInt() ifadesi sayıyı integer'a dönüştürmeye yarar.
+  var deger = 58;
+  deger =53.36.toInt();
+  print(deger);
+}
