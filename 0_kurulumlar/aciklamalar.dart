25dca862dde2ec25c93fb0d02de98e8278c66abc
Kürşat Emre Fındık <147005854+kursatfndk@users.noreply.github.com>
2025-09-22 21:03:27 +0300

    Create aciklamalar.dart

---
+++ 0_kurulumlar/aciklamalar.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  print("Kürşat Emre Fındık'ın dersleri");
+  print("Dart Öğrenmek çok keyifli");
+  print('Dart');
+  print('Emre\'nin dersleri');
+  print("3 * 5");
+  print(3 * 5);
+  print(3 + 5);
+  //Bu bölgeye yazılan kelimer yorum satırıdır.
+  print("kürşat " + "Emre");
+
+  /* Yorum Bloğu
+  print("deneme")
+  print("deneme")
+  print("deneme")
+  */
+}
