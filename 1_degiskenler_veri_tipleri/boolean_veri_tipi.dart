f8589f1c1a4dc5e982567938b10fd4049516669c
Kürşat Emre Fındık <147005854+kursatfndk@users.noreply.github.com>
2025-09-22 22:12:00 +0300

    Create boolean_veri_tipi.dart

---
+++ 1_degiskenler_veri_tipleri/boolean_veri_tipi.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  //bool veri tipi doğru yanlış şeklinde ifade edilir.
+  bool ogrenciMi = false;
+  print(ogrenciMi);
+  bool calisiyorMu = true;
+  print(calisiyorMu);
+
+  var ehliyatVarMi = true;
+  print(ehliyatVarMi);
+}
