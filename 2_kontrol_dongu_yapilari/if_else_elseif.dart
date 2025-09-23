a594a71162da0e60be05a02156bb786689eaba3c
Kürşat Emre Fındık <147005854+kursatfndk@users.noreply.github.com>
2025-09-23 18:16:21 +0300

    Create if_else_elseif.dart

---
+++ 2_kontrol_dongu_yapilari/if_else_elseif.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  int sayi1 = 15, sayi2 = 8;
+
+  /* if (sayi1 > sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısından büyüktür");
+  } else {
+    print("$sayi1 sayısı $sayi2 sayısından küçüktür");
+  } */
+  //İF BLOĞUNDA ÇALIŞACAK KISIMLARI {}SÜSLÜ PARANTEZ İÇİNDE YAZ!
+  /* if (sayi1 > sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısından büyüktür");
+  } else if (sayi1 < sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısından küçüktür");
+  } else {
+    print("$sayi1 sayısı $sayi2 sayısına eşittir");
+  } */
+
+  if (sayi1 > sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısından büyüktür");
+  }
+  if (sayi1 < sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısından küçüktür");
+  }
+  if (sayi1 == sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısına eşittir");
+  }
+}
