f8924be1c04987e74a9506cc3ee067def4ce3de7
Kürşat Emre Fındık <147005854+kursatfndk@users.noreply.github.com>
2025-09-23 21:31:54 +0300

    Update number_data_type.dart

--- 1_variables_data_types/number_data_type.dart
+++ 1_variables_data_types/number_data_type.dart
@@ -1,22 +1,22 @@
 void main(List<String> args) {
-  int yas = 23;
-  print(yas);
-  //Pythonda float tipi burada double
-  double sicaklik = 50.7;
-  print(sicaklik);
-  //num hem tam sayı hemde ondalıklı  sayıyı alır.
+  int age = 23;
+  print(age);
+  //The float type in Python is double here.
+  double temperature = 50.7;
+  print(temperature);
+  //‘num’ takes both integer and decimal numbers.
   num kilo = 70;
   print(kilo);
 
   num kilo1 = 70.8;
   print(kilo1);
- // var ifadesi variable ifadesinin kısaltılmışıdır.
- // değişken türünü belirtmeden işlem yapma imkanı sağlar.
- // veriyi tanır ve öyle işleme başlar.
-  var yil = 1995;
-  print(yil);
-  // .toInt() ifadesi sayıyı integer'a dönüştürmeye yarar.
-  var deger = 58;
-  deger =53.36.toInt();
-  print(deger);
+ // The word ‘var’ is an abbreviation for the word ‘variable’..
+ // enables operations to be performed without specifying the variable type.
+ // recognises the data and begins processing it accordingly.
+  var year = 1995;
+  print(year);
+  // The .toInt() expression converts the number to an integer.
+  var value = 58;
+  value =53.36.toInt();
+  print(value);
 }
