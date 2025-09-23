44d6c0cef6a360dbd0b0fdda1eb8976787a26618
Kürşat Emre Fındık <147005854+kursatfndk@users.noreply.github.com>
2025-09-23 21:26:44 +0300

    Update end-of-chapter_questions.dart

--- 1_variables_data_types/end-of-chapter_questions.dart
+++ 1_variables_data_types/end-of-chapter_questions.dart
@@ -1,23 +1,23 @@
 /*
-1- Bir üçgenin tüm kenarlarını değişkende saklayın ve çevresini hesaplayıp ekrana yazdırın.
-Örnek çıktı : birinci kenarı 3, ikinci kenarı 4, üçüncü kenarı 5 olan üçgenin çevresi 12dir.
+1-Store all sides of a triangle in variables and calculate its perimeter, then print it to the screen.
+Example output: The perimeter of a triangle with the first side being 3, the second side being 4, and the third side being 5 is 12.
 
-2-Adınızı, doğum yılınızı, güncel yılı değişkenlerde saklayıp yaşınızı ekrana yazdırın
-Örnek çıktı : adım emre, 1988 yılında doğdum ve şuan 35 yaşındayım.
+2-Store your name, year of birth, and current year in variables and display your age on the screen.
+Example output: My name is Emre, I was born in 1988, and I am currently 35 years old.
 */
 
 void main(List<String> args) {
-  var birinciKenar = 3;
-  var ikinciKenar = 4;
-  var ucuncuKenar = 5;
+  var firstSideBeing = 3;
+  var secondSideBeing = 4;
+  var thirdSideBeing = 5;
   print(
-    "Üçgenin birinci kenarı $birinciKenar , üçgenin ikinci kenarı $ikinciKenar , üçgenin üçüncü kenarı $ucuncuKenar ve çevresi toplam : ${birinciKenar + ikinciKenar + ucuncuKenar}",
+    "The first side of the triangle is $firstSideBeing, the second side of the triangle is $secondSideBeing, the third side of the triangle is $thirdSideBeing, and the total perimeter is: ${firstSideBeing + secondSideBeing + thirdSideBeing}",
   );
 
-  var ad = "Kürşat";
-  var dogumYili = 2002;
-  var guncelYil = 2025;
+  var name = "Kürşat";
+  var yearOfBirth = 2002;
+  var currentYear = 2025;
   print(
-    "adım $ad, $dogumYili yılında doğdum ve şuan ${guncelYil - dogumYili} yaşındayım.",
+    "My name is $name, I was born in $yearOfBirth and I am currently ${currentYear - yearOfBirth} years old.",
   );
 }
