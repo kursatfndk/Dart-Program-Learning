bc5ec217428f2d1e8affa4a6b665253ed7bfde22
Kürşat Emre Fındık <147005854+kursatfndk@users.noreply.github.com>
2025-09-27 01:51:43 +0300

    Update data_collection_users.dart

--- data_collection_users.dart
+++ data_collection_users.dart
@@ -1,11 +1,1 @@
-import 'dart:io';
 
-void main(List<String> args) {
-  print("Please enter your name.");
-  var name = stdin.readLineSync();
-  print("The name value you entered: $name");
-
-  print("Please enter your age.");
-  var age = int.parse(stdin.readLineSync()!);
-  print("The age value you entered: $age");
-}
